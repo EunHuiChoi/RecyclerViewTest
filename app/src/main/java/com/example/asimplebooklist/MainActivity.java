fbe0785670e13f4fcc1b3107203a87dcf73eefe1
EunHuiChoi <eun95828@likelion.org>
2019-10-18 03:09:39 +1100

    third incomplete

--- app/src/main/java/com/example/asimplebooklist/MainActivity.java
+++ app/src/main/java/com/example/asimplebooklist/MainActivity.java
@@ -27,4 +27,8 @@ public class MainActivity extends AppCompatActivity {
         adapter = new RecyclerAdapter();
         recyclerView.setAdapter(adapter);
     }
+
+    private void getData() {
+        
+    }
 }
